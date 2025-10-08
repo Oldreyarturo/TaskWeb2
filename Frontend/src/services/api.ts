3141dba3c7d07d485eaca7d3db3a8ea371b4c72c
Arturo Michel <michelorodriguez2004@gmail.com>
2025-10-08 01:56:20 -0600

    modificamos el ngrok del Api

--- Frontend/src/services/api.ts
+++ Frontend/src/services/api.ts
@@ -2,7 +2,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
-const API_BASE_URL = ' https://literary-eliseo-geniculate.ngrok-free.dev/api';
+const API_BASE_URL = 'https://superwealthy-numerously-collin.ngrok-free.dev/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
